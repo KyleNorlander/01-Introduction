0b4f743fc16eeadde88c64b8dfd7a087d32fad96
Juandonboolay <54556328+Juandonboolay@users.noreply.github.com>
2019-08-29 17:07:39 -0400

    Added a few answers

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hi, my name is Kyle Norlander')
+print('Hello, my name is Kyle Norlander and I was born in the northland of Minnesota')
+print('Choosing a favorite game is nearly impossible for me but if I had to pick then I would say Fallout 3 for single player, and the Battlefield franchise for multiplayer!')
+print('My general concern for this class would be having zero experience in programming while also wanting to go above and beyond the set standards')
+print('I am very very excited just to even dip my toes into the world of programming. Although it is not my desired career path, I still find it incredibly interesting and believe having a wide skillset will be of utmost importance in the game design world.)
+print('')
