709a8950428882dac507f7e23986d8b1264cfa0d
Juandonboolay <54556328+Juandonboolay@users.noreply.github.com>
2019-08-29 18:27:54 -0400

    Finished questions, now to stylize

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ print('Hello, my name is Kyle Norlander and I was born in the northland of Minne
 print('Choosing a favorite game is nearly impossible for me but if I had to pick then I would say Fallout 3 for single player, and the Battlefield franchise for multiplayer!')
 print('My general concern for this class would be having zero experience in programming while also wanting to go above and beyond the set standards')
 print('I am very very excited just to even dip my toes into the world of programming. Although it is not my desired career path, I still find it incredibly interesting and believe having a wide skillset will be of utmost importance in the game design world.)
-print('')
+print('My stackoverflow.com user number is 11980659')
+print('The URL to my github.com profile is https://github.com/KyleNorlander')
